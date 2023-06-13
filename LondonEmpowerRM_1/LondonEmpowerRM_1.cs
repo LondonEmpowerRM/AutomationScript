8c99a1236ad252891559e228115651a56005a841
Razvan Mielcioiu <razvan-paul.mielcioiu@ericsson.com>
2023-06-13 14:24:48 +0100

    changed inforation event message

--- LondonEmpowerRM_1/LondonEmpowerRM_1.cs
+++ LondonEmpowerRM_1/LondonEmpowerRM_1.cs
@@ -17,7 +17,7 @@ namespace LondonEmpowerRM_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello World from Empower London");
 		}
 	}
 }
