d76af331952aba0b83230021d4d4d3cf07abe21b
Razvan Mielcioiu <razvan-paul.mielcioiu@ericsson.com>
2023-06-13 14:20:52 +0100

    Add project files.

---
+++ LondonEmpowerRM_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LondonEmpowerRM_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("LondonEmpowerRM_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C09CE4A9-B52C-4C04-B917-03D8CBA1C75B")]
+[assembly: AssemblyVersion("1.0.0.0")]
